90edfee47d3326fdcbc0d994fc3b9f007e89b9b9
aline28aguilar <aline.28aguilar@gmail.com>
2026-09-13 16:01:32 -0600

    Fix Medusa config structure

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -6,13 +6,13 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
-  },
 
-  http: {
-    storeCors: process.env.STORE_CORS!,
-    adminCors: process.env.ADMIN_CORS!,
-    authCors: process.env.AUTH_CORS!,
-    jwtSecret: process.env.JWT_SECRET!,
-    cookieSecret: process.env.COOKIE_SECRET!,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET!,
+      cookieSecret: process.env.COOKIE_SECRET!,
+    },
   },
 })
